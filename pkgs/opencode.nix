d8fc2eecca728da1681b509c292eb1b48ddfa8fa
Wes Payne <noblepayne@noblepayne.com>
2026-03-13 13:58:33 -0700

    opencode: 1.2.21 -> 1.2.26
    Diff: https://github.com/anomalyco/opencode/compare/v1.2.21...v1.2.26

--- pkgs/opencode.nix
+++ pkgs/opencode.nix
@@ -4,11 +4,11 @@
   fetchzip,
   patchelf,
 }: let
-  version = "1.2.21";
+  version = "1.2.26";
   srcs = {
     "x86_64-linux" = fetchzip {
       url = "https://github.com/anomalyco/opencode/releases/download/v${version}/opencode-linux-x64-baseline.tar.gz";
-      hash = "sha256-Wse7VIqxpKwG5wPt87vqEdJ67xI+J8fKrdUHsehfaRQ=";
+      hash = "sha256-zMIc7Jd1Eyw0kX2EJTA7CazspWuz4vkX49PtNvWYJWk=";
       stripRoot = false;
     };
     "aarch64-linux" = fetchzip {
